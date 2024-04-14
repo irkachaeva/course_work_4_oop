60e48e788e3d5857cc47dbe1a96ce7e22690a571
ira_kachaeva <irenkachaeva@gmail.com>
2024-04-14 15:26:41 +0300

    работа с API сервиса с вакансиями.  реализован класс, наследующийся от абстрактного класса, для работы с платформой hh.ru. подключается к API и получает вакансии.

---
+++ src/Vacancy.py
@@ -0,0 +1,11 @@
+class Vacancy:
+    def __init__(self, name, salary, area, description, employer, experience, type):
+        self.name = name
+        self.salary = salary
+        self.area = area
+        self.description = description
+        self.employer = employer
+        self.experience = experience
+        self.type = type
+
+    pass
