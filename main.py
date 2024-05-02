8356a363888ffce1727c6bd09907dc9b1cbe0383
ikachaeva <irenkachaeva@gmail.com>
2024-05-02 19:53:08 +0300

    Исправила расчет вывода ЗП гросс в классе вакансии

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from src import utils
 import os.path
 from src.utils import get_start
+from src.HeadHunterAPI import HeadHunterAPI
+from src.Vacancy import Vacancy
+from src.JSONSaver import JSONSaver
+import json
+import os.path
 
 if __name__ == '__main__':
-    get_start()
+     get_start()
 
